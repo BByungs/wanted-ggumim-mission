4fc4f54678eae334b879d14f8114a33124cb9ac5
Ahn <81910935+BYEONGJIN-AHN@users.noreply.github.com>
2022-02-01 20:58:29 +0900

    refactor: InfoInPhoto refactoring

--- src/components/InfoInPhoto.jsx
+++ src/components/InfoInPhoto.jsx
@@ -1,43 +1,11 @@
-import styled from 'styled-components';
+import Img from 'common/Img';
+import Wrapper from 'common/Wrapper';
+import { Background, ImgWrapper, ScrollWrapper } from 'styles/InfoInPhotoStyle';
 import ImageList from './ImageList';
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const ScrollWrapper = styled.div`
-  width: 800px;
-  overflow-x: scroll;
-`;
-
-const Background = styled.div`
-  background-image: url(${(props) => props?.image});
-  background-position: center center;
-  background-size: contain;
-  width: 800px;
-  height: 998.39px;
-  position: relative;
-  display: inline-block;
-  cursor: pointer;
-`;
-
-const ImgWrapper = styled.div`
-  position: absolute;
-  display: block;
-  cursor: pointer;
-  left: ${(props) => `${props.pointY * 1.7}`}px;
-  top: ${(props) => `${props.pointX * 1.55}`}px;
-`;
-
-const Img = styled.img`
-  width: 32px;
-  height: 32px;
-`;
 
 const InfoInPhoto = ({ image, data }) => {
   return (
-    <Wrapper>
+    <Wrapper style={{ flexDirection: 'column' }}>
       <Background image={image}>
         {data?.productList.map((el) => (
           <ImgWrapper key={el.productId} pointX={el.pointX} pointY={el.pointY}>
@@ -46,6 +14,7 @@ const InfoInPhoto = ({ image, data }) => {
                 'https://cdn.ggumim.co.kr/storage/20211029145238AlZrQ41xtg.png'
               }
               alt={el.productName}
+              style={{ width: '32px', height: '32px' }}
             />
           </ImgWrapper>
         ))}
