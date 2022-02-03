9c1ea52f0a985a76d2302efde3c768bafbfd79c1
Ahn <81910935+BYEONGJIN-AHN@users.noreply.github.com>
2022-02-04 02:22:51 +0900

    refactor: InfoInPhoto 감싸는 Wrapper적용 , import

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
-import axios from "axios";
-import InfoInPhoto from "components/InfoInPhoto";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import {InfoInPhoto , InfoInPhotoWrapper} from "components"
+import { getApi } from "utils/utilFuncs"
 import GlobalStyle from "styles/GlobalStyle";
 
 const App = () => {
   const [data, setData] = useState(null);
-  const getApi = () => {
-    axios
-      .get("https://cdn.ggumim.co.kr/test/image_product_link.json")
-      .then((res) => {
-        if (!data) {
-          res.data.productList.forEach((product) => {
-            product.focus = false;
-          });
-          setData(res.data);
-        }
-      })
-      .catch((err) => alert(err));
-  };
 
   useEffect(() => {
-    getApi();
+    getApi(data, setData);
   }, []);
 
   return (
-    <React.Fragment>
+    <>
       <GlobalStyle />
-      <InfoInPhoto image={data?.imageUrl} data={data} setData={setData} />
-    </React.Fragment>
+      <InfoInPhotoWrapper>
+        <InfoInPhoto image={data?.imageUrl} data={data} setData={setData} />
+      </InfoInPhotoWrapper>
+    </>
   );
 };
 
