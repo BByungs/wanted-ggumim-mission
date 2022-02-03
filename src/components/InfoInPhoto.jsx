876708887da8ee2c6c38715638d993852ab97941
Ahn <81910935+BYEONGJIN-AHN@users.noreply.github.com>
2022-02-04 02:26:14 +0900

    refactor: utils함수로 뺄수있는 함수들은 빼고, import 간결하게 정리

--- src/components/InfoInPhoto.jsx
+++ src/components/InfoInPhoto.jsx
@@ -1,32 +1,9 @@
-import Img from 'common/Img';
-import Wrapper from 'common/Wrapper';
-import React from 'react';
+import {Img, Wrapper} from "common";
+import {ImageList, Tooltip} from "components";
+import { handleMagnifier, handleClose } from "utils/utilFuncs";
 import { Background, ImgWrapper, ScrollWrapper } from 'styles/InfoInPhotoStyle';
-import ImageList from './ImageList';
-import Tooltip from './Tooltip';
 
 const InfoInPhoto = ({ image, data, setData }) => {
-  const handleClose = (idx) => {
-    const copyData = { ...data };
-    copyData.productList.forEach((el, productIdx) => {
-      if (productIdx === idx) {
-        el.focus = false;
-      }
-    });
-    setData(copyData);
-  };
-
-  const handleMagnifier = (idx) => {
-    const copyData = { ...data };
-    copyData.productList.forEach((el, productIdx) => {
-      if (productIdx === idx) {
-        el.focus = true;
-      } else {
-        el.focus = false;
-      }
-    });
-    setData(copyData);
-  };
 
   const handleBackground = (e) => {
     if (e.target.localName === 'div' && e.target.childNodes.length >= 7) {
@@ -44,12 +21,12 @@ const InfoInPhoto = ({ image, data, setData }) => {
         {data?.productList.map((el, idx) => (
           <ImgWrapper key={el.productId} pointX={el.pointX} pointY={el.pointY}>
             {el.focus ? (
-              <React.Fragment>
+              <>
                 <Img
                   src={
                     'https://cdn.ggumim.co.kr/storage/20211029145330GwwumnWNSs.png'
                   }
-                  onClick={() => handleClose(idx)}
+                  onClick={() => handleClose(idx,data,setData)}
                   alt="close"
                   style={{
                     width: '32px',
@@ -65,13 +42,13 @@ const InfoInPhoto = ({ image, data, setData }) => {
                   pointX={el.pointX}
                   pointY={el.pointY}
                 />
-              </React.Fragment>
+              </>
             ) : (
               <Img
                 src={
                   'https://cdn.ggumim.co.kr/storage/20211029145238AlZrQ41xtg.png'
                 }
-                onClick={() => handleMagnifier(idx)}
+                onClick={() => handleMagnifier(idx,data,setData)}
                 alt="magnifier"
                 style={{ width: '32px', height: '32px' }}
               />
