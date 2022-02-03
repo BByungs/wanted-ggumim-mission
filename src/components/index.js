d14a0f8b6252dbe76e5c59528c1834be24e86843
Ahn <81910935+BYEONGJIN-AHN@users.noreply.github.com>
2022-02-04 02:28:39 +0900

    add: import 한줄로 할수있게 index.js 추가

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import ImageList from "./ImageList";
+import InfoInPhoto from "./InfoInPhoto";
+import InfoInPhotoWrapper from  "./InfoInPhotoWrapper";
+import Tooltip from "./Tooltip";
+
+export {
+    ImageList,
+    InfoInPhoto,
+    InfoInPhotoWrapper,
+    Tooltip,
+};
