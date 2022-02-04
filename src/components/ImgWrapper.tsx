58c5ed8a2c6f36bc9e330e62aac997f42e23b41d
Ahn <81910935+BYEONGJIN-AHN@users.noreply.github.com>
2022-02-05 04:58:38 +0900

    refactor: 필요없는 프롭스 제거, 코드스타일 변경

--- src/components/ImgWrapper.tsx
+++ src/components/ImgWrapper.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-const Wrapper: any = styled.div`
+const Wrapper = styled.div`
   display: flex;
   position: relative;
   margin: 25px 5px 10px 10px;
@@ -9,13 +9,11 @@ const Wrapper: any = styled.div`
   height: 106px;
   cursor: pointer;
   z-index: 0;
-  border: ${(props: ImgWrapperProps) =>
-    !props.focus ? '2px solid #b2b6bc' : '2px solid #e77596'};
+  border: ${({ focus }: { focus: boolean }) =>
+    !focus ? '2px solid #b2b6bc' : '2px solid #e77596'};
 `;
 
 interface ImgWrapperProps {
-  id: number;
-  tabIndex: string;
   focus: boolean;
   children: React.ReactNode;
   onClick: React.MouseEventHandler;
@@ -23,12 +21,7 @@ interface ImgWrapperProps {
 
 const ImgWrapper = (props: ImgWrapperProps): JSX.Element => {
   return (
-    <Wrapper
-      id={props.id}
-      tabIndex={props.tabIndex}
-      focus={props.focus}
-      onClick={props.onClick}
-    >
+    <Wrapper focus={props.focus} onClick={props.onClick}>
       {props.children}
     </Wrapper>
   );
